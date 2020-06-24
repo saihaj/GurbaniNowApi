e9a591af272e425230396668201476d71b030837
saihaj <44710980+saihaj@users.noreply.github.com>
2020-06-24 15:16:50 -0500

    feat: shabads query

--- src/graphql/resolvers/index.js
+++ src/graphql/resolvers/index.js
@@ -1,10 +1,12 @@
 import banis from './banis'
 import meta from './meta'
+import shabad from './shabad'
 
 const rootResolver = {
   Query: {
     ...banis,
     ...meta,
+    ...shabad,
   },
 }
 export default rootResolver
